e6bce06bb5b8544cd002ad1245431fce7a52c873
Yang Zhou <yangzhou@g.harvard.edu>
2021-01-25 13:41:33 -0800

    update pull_update to use https

--- pull_update.sh
+++ pull_update.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-git remote add upstream "git@github.com:Harvard-CS145/cs145-21-project0.git"
+git remote add upstream "https://github.com/Harvard-CS145/cs145-21-project0.git"
 git pull upstream main --allow-unrelated-histories
